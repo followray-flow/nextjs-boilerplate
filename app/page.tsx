771e51d2ab71ff7d5518a407e78b44b6c6b7f7de
followray-flow <followray.woo@gmail.com>
2026-01-28 12:44:48 +0000

    feat: 문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다!잘 할수 있어요. 그래요. 잘할거에요. 반드시 성공</h1>
+      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다!잘 할수 있어요. 그래요. 잘할거에요. 우상규는 반드시 성공</h1>
     </div>
   );
 }
