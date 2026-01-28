a1ef7a910623ddc1d4692d03ca07cae262ee1d91
followray-flow <followray.woo@gmail.com>
2026-01-28 07:15:43 +0000

    feat: 제대로 하는건가

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다!잘 할수 있어요. 반드시 성공</h1>
+      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다!잘 할수 있어요. 그래요. 반드시 성공</h1>
     </div>
   );
 }
