f17f9ab6efc5a89d19d30071cbe32ddd3b575193
followray-flow <followray.woo@gmail.com>
2026-01-28 06:57:40 +0000

    feat: Hello World 메시지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>안녕하세요. 프로그래밍의 세계로 오신 것을 환영합니다!우상규씨 잘해봅시다. 최선을 다합시다. 반드시 성공</h1>
+      <h1>안녕하세요. 프로그래밍의 세계로 오신 것을 환영합니다!우상규씨 잘해봅시다. 최선을 다합시다. 잘 할수 있어요. 반드시 성공</h1>
     </div>
   );
 }
