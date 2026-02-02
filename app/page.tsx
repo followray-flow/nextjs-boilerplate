dc260fa1d518206c4f67eef4ba8bcd51a1580d10
상규 <followray.woo@gmail.com>
2026-02-02 21:26:08 +0900

    feat: 2일차 시작

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다!</h1>
+      <h1>안녕하세요. 상규씨. 프로그래밍의 세계로 오신 것을 환영합니다! 잘 할 수 있어요. 힘내세요</h1>
     </div>
   );
 }
